29bd2b208ee29d9b77108a96fab186390000ffe3
Antonio <antonioalvino19@gmail.com>
2019-12-31 12:56:08 +0100

    read file exercise

--- myex09_1.py
+++ myex09_1.py
@@ -11,4 +11,24 @@ def read_file(filename, num):
             print(x)
 
 
-print(read_file("/Users/antonioalvino/Downloads/words.txt", 20))
+# print(read_file("/Users/antonioalvino/Downloads/words.txt", 20))
+
+"""
+Write a function called has_no_e that returns True if the given word doesn’t have the letter “e” in it.
+Write a program that reads words.txt and prints only the words that have no “e”.
+Compute the percentage of words in the list that have no “e”.
+"""
+
+
+def has_no_e(word):
+    return True if word.count("e") == 0 else False
+
+
+def read_file_no_e(filename):
+    fin = open(filename)
+    for x in fin:
+        if has_no_e(x):
+            print(x)
+
+
+print(read_file_no_e("/Users/antonioalvino/Downloads/words.txt"))
